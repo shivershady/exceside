4a239df6f2178507a20b07d267abac2a817f5e4f
Windows 11 <shivershady@gmail.com>
2022-04-05 04:01:22 -0600

    update

---
+++ src/components/Banner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="banner"></div>
+</template>
+
+<script>
+export default {
+  name: `Banner`
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header"></div>
+</template>
+
+<script>
+export default {
+  name: `Header`,
+  data() {
+    return {
+      img:[
+        {logo: require('../assets/nongthao-1200x230 1.svg')}
+      ]
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
