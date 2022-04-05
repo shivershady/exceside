44e63f603e65b5362c8b7266dae75c681b5ef1fa
Hoàng Hiếu <shivershady@gmail.com>
2022-04-06 00:47:24 +0700

    update

---
+++ src/components/dropdown.vue
@@ -0,0 +1,38 @@
+<template lang="">
+  <section class="dropDownMenuWrapper">
+    <button class="dropDownMenuButton" ref="menu" @click="openClose">
+      {{ menuTitle }}
+    </button>
+
+    <div class="iconWrapper">
+      <div class="bar1" />
+      <div class="bar2" />
+      <div class="bar3" />
+    </div>
+
+    <section class="dropdownMenu" v-if="isOpen">
+      <div class="menuArrow" />
+      <slot />
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  props: [ "menuTitle" ],
+  data() {
+    return {
+      isOpen: false
+  },
+  methods: {
+
+    openClose() {
+
+      isOpen = !isOpen
+
+    }
+
+  }
+};
+</script>
+<style lang=""></style>
